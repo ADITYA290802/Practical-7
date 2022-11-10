fae4efaf219428bde873b334a88598c29f7df6df
HetviPatel27 <77675811+HetviPatel27@users.noreply.github.com>
2022-11-10 10:14:34 +0530

    Initial commit

---
+++ app/src/main/java/com/firstapp/prac_7_076/MyBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package com.firstapp.prac_7_076
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val i = Intent(context, AlarmOnActivity::class.java)
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context?.startActivity(i)
+    }
+}
